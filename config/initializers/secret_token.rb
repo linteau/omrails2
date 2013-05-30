e2dee8faf018213a3ab43834d9c1e7c4ab6cb05c
andylinteau <andy.linteau@buystand.com>
2013-05-30 13:31:40 +0800

    create new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails2::Application.config.secret_token = '968a91a2d3714fc9ed88182a78ad17122c8e2d2bbab0e35eaef2f114368d7b915d78600824b7b5d1cd1e4ba5bf286772519144bad37a7b9a117a88bb4c82b75b'
